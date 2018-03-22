3b155f7a61f4e981ec30915fe2f044e3bdb1e4f9
Hans Tzou <hanstzou@users.noreply.github.com>
2018-03-23 00:45:17 +0800

    add daodu, hd1069, verywed, google, babyhome, kiwi-ad, and opensignal

--- bin/gen_filter.sh
+++ bin/gen_filter.sh
@@ -1,4 +1,5 @@
 bin/1bpkg_to_filter.py 
+cat static_list.txt >> tw_float_list.txt
 ../YousList/bin/add_checksum.py tw_float_list.txt > a.txt && mv a.txt tw_float_list.txt
 ../YousList/bin/validate_checksum.py tw_float_list.txt 
 
